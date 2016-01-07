bbfc4bb32bdf40edfcb5da5787c2e658154e214b
hotator <stephan.besecke@st.ovgu.de>
2016-01-07 06:43:02 +0100

    update autoclust, but it has issues -.-

--- cluster/Autoclust.py
+++ cluster/Autoclust.py
@@ -77,7 +77,22 @@ class Autoclust(Cluster):
                 else:
                     self.other_edges_list += [(i, p)]
 
+    def get_matrix(self):
+        m = np.zeros((len(self.points), len(self.points)))
+
+        for t in self.edge_list:
+            x, y = min(t), max(t)
+            m[(x, y)] = 1
+
+        return m
+
+    def gen_labels(self):
+        m = self.get_matrix()
+        g = nx.from_numpy_matrix(m)
+        self.labels = list(nx.connected_components(g))
+
     def phase1(self):
+        print('phase1')
         self.gen_point_infos()
         self.local_mean()
         self.local_st_dev()
@@ -95,21 +110,60 @@ class Autoclust(Cluster):
             #elif (t[1], t[0]) in self.edge_list:
             #    self.edge_list.remove((t[1], t[0]))
 
-        self.plot_points()
+    def get_short_points(self, i):
+        return [x[1] for x in self.short_edges_list if x[0] == i] #+ [x[0] for x in self.short_edges_list if x[1] == i]
 
+    def get_other_points(self, i):
+        return [x[1] for x in self.other_edges_list if x[0] == i] #+ [x[0] for x in self.other_edges_list if x[1] == i]
+
+    def plot_edge_list(self):
+        self.plot_points()
         for i, j in self.edge_list:
             x, y = zip(*[self.points[i], self.points[j]])
             plt.plot(x, y)
         plt.show()
 
     def phase2(self):
-        # get connected components from self.edge_list
-        pass
+        print('phase2')
+        self.gen_labels()
+
+        for i in range(len(self.points)):
+            # 1. phase
+            short_list = self.get_short_points(i)
+            if short_list:
+                # FIXME: if equal -> NOT the first but that one with shortest edge!
+                c_label = self.labels.index(max([x for x in self.labels for n in short_list if n in x], key=len))
+
+                # 2. phase
+                other_list = self.get_other_points(i)
+                if other_list:
+                    if [self.labels.index(s) for s in self.labels if other_list[0] in s][0] != c_label:
+                        for v in other_list:
+                            if (i, v) in self.edge_list:
+                                self.edge_list.remove((i, v))
+                            #elif (v, i) in self.edge_list:
+                            #    self.edge_list.remove((v, i))
+
+                # 3. phase
+                for v in short_list:
+                    if v in self.labels[c_label]:
+                        self.edge_list += [(i, v)]
+
+    def get_last_points(self, i):
+        return [x[1] for x in self.edge_list if x[0] == i] #+ [x[0] for x in self.edge_list if x[1] == i]
 
     def phase3(self):
-        pass
+        print('phase3')
+        for i in range(len(self.points)):
+            last_list = self.get_last_points(i)
+            for v in last_list:
+                if self.local_mean_list[i] + self.mean_st_dev_val < mydist(self.points[i], self.points[v]):
+                    self.edge_list.remove((i, v))
 
     def calculate(self):
         self.phase1()
+        #self.plot_edge_list()
         self.phase2()
+        #self.plot_edge_list()
         self.phase3()
+        self.plot_edge_list()
